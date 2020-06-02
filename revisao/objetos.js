63e12c7863d66c811419a7090e2108dd4cb95d62
unknown <sarafogo.ss@gmail.com>
2020-06-02 09:25:31 -0300

    exercicio encaminhado

--- revisao/objetos.js
+++ revisao/objetos.js
@@ -10,6 +10,45 @@ console.log("Personalidade1: ", pessoa.personalidade[0]);
 
 //exercício
 
+const cliente1 = {
+ 
+  Nome: 'Dora Barreto',
+	CPF: '223.001.221-29',
+	valorDaCompra: '2000',
+  modeloDoCelular: 'XXX-12',
+  AnoDoCelular: 2020,
+	corDoCelular: 'Amarelo',
+	loja: 'RJ-BR',
+	atendente: 'Joana Maria' 
+};
+
+
+	const cliente2 = {
+
+    
+    Nome: 'Marcela Janino Silva',
+    CPF: '321.101.231-29',
+    valorDaCompra: '1300',
+    modeloDoCelular: 'XXM-y',
+    anoDoCelular: 2019,
+    corDoCelular: 'ROXO',
+    loja: 'SP-BR',
+    atendente: 'Drielly Gomes Almeida'
+  }; 
+
+
+const	cliente3 = {
+
+  
+  Nome: 'Raquel Borges Figueredo',
+	CPF: '910.311.425-01',
+	valorDaCompra: '4000',
+	modeloDoCelular: 'MDM-PRO',
+	anoDoCelular: 2020,
+	corDoCelular: 'AZUL',
+	loja: 'MG-BR',
+	atendente: 'Fran Carneiro de Pinha'
+}; 
 
 
 
